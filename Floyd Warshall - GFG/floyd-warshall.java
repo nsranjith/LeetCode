c3f56a563d5c8a13e0f951255368d66b172ef728
nsranjith <nsranjith123@gmail.com>
2023-08-13 19:26:28 +0530

    Added solution - LeetHub

---
+++ Floyd Warshall - GFG/floyd-warshall.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] matrix = new int[n][n];
+            for(int i = 0; i < n; i++){
+                String[] s = br.readLine().trim().split(" ");
+                for(int j = 0; j < n; j++)
+                    matrix[i][j]  =Integer.parseInt(s[j]);
+            }
+            Solution obj = new Solution();
+            obj.shortest_distance(matrix);
+            for(int i = 0; i < n; i++){
+                for(int j  = 0; j < n; j++){
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    public void shortest_distance(int[][] matrix)
+    {
+        // Code here 
+        int N=matrix.length;
+        for(int i=0;i<N;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                  if(matrix[i][j]==-1){
+                      matrix[i][j]=(int)Math.pow(10,8);
+                  }
+                  if(i==j) matrix[i][j]=0;
+            }
+        }
+        for(int k=0;k<N;k++){
+            for(int i=0;i<N;i++){
+                for(int j=0;j<matrix[i].length;j++){
+                  matrix[i][j]=Math.min(matrix[i][j], matrix[i][k]+matrix[k][j]);        
+                }
+            }
+        }
+        for(int i=0;i<N;i++){
+            for(int j=0;j<matrix[i].length;j++){
+              if(matrix[i][j]==(int)Math.pow(10,8)){
+                  matrix[i][j]=-1;
+              }      
+            }
+        }
+    }
+}
