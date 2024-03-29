f5e1707bd955e70d23f07927b3dc12a3dedc8801
nsranjith <nsranjith123@gmail.com>
2024-03-29 20:19:02 +0530

    Time: 1 ms (11.31%), Space: 41.2 MB (5.20%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,33 +1,31 @@
 class Solution {
     int dp[][];
-    int ans=0;
     public int uniquePaths(int m, int n) {
+        int grid[][]=new int[m][n];
+        int vis[][]=new int[m][n];
         dp=new int[m][n];
-        for(int rows[]:dp){
-            Arrays.fill(rows,-1);
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
         }
-        int visited[][]=new int[m][n];
-        return helper(m,n,0,0,visited);
+        return helper(grid,0,0,vis);
     }
-    private int helper(int m,int n,int i, int j, int[][] visited){
-        int rows[]={1,0};
-        int cols[]={0,1};
-        int ans=0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(i==m-1 && j==n-1){
+    private int helper(int grid[][], int m, int n,int vis[][]){ 
+        if(m==grid.length-1 && n==grid[0].length-1){
             return 1;
         }
-        for(int k=0;k<2;k++){
-            int nr=rows[k]+i;
-            int nc=cols[k]+j;
-            if(nr<m && nc<n && nr>=0 && nc>=0 && visited[nr][nc]!=1){
-                visited[nr][nc]=1;
-                ans+=helper(m,n,nr,nc,visited);
-                visited[nr][nc]=0;
+        int rows[]={1,0};
+        int cols[]={0,1};
+        vis[m][n]=1;
+        int res=0;
+        if(dp[m][n]!=-1) return dp[m][n];
+        for(int i=0;i<2;i++){
+            int nr=m+rows[i];
+            int nc=n+cols[i];
+            if(nr<grid.length && nc<grid[0].length && nr>=0 && nc>=0 && vis[nr][nc]!=1) {
+              res += helper(grid,nr,nc,vis);
             }
-            
         }
-        
-        return dp[i][j]=ans;
+        vis[m][n]=0;
+        return dp[m][n]=res;
     }
 }
