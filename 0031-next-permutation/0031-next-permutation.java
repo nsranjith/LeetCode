10748238af03a1895ca6be42d85973c9db29ab3b
nsranjith <nsranjith123@gmail.com>
2024-03-29 16:08:18 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (90.27%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,60 +1,37 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-     /*
-        1 2 3
-        index1=1
-        index 2=2
-        1 3 2
-
-
-     */
-     
-     
-       int pos=-1;
-       for(int i=nums.length-2;i>=0;i--){
-           if(nums[i]<nums[i+1]){
-               pos=i;
-               break;
-           }
-       }
-       
-       if(pos==-1){
-
-           /*
-                3 2 1
-                index 1
-
-           */
-           int i=0;
-           int j=nums.length-1;
-           while(i<j){
-            int temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
-            i++;
-            j--;
-         }
-         return;
-       } // already in sorted and last one
-
-       int pos1=-1;
-       for(int i=nums.length-1;i>=0;i--){
-           if(nums[i]>nums[pos]){
-             pos1=i;
-             break;
+    
+        int pos=-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                pos=i;
+                break;
             }
-       }
-       int temp=nums[pos];
-       nums[pos]=nums[pos1];
-       nums[pos1]=temp;
-       int j=nums.length-1;
-       int i=pos+1;
-       while(i<j){
-        temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
-        i++;
-        j--;
-       }
+        }
+        if(pos==-1){
+            reverse(nums,0);
+            return;
+        }
+        // 1 3 2 4
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i]>nums[pos]){
+               int temp=nums[i];
+               nums[i]=nums[pos];
+               nums[pos]=temp;
+               break;
+            }
+        }
+        reverse(nums,pos+1);
+    }
+    private void reverse(int nums[], int index){
+        int i=index;
+        int j=nums.length-1;
+        while(i<=j){
+             int temp=nums[i];
+             nums[i]=nums[j];
+             nums[j]=temp;
+             i++;
+             j--;
+        }
     }
 }
